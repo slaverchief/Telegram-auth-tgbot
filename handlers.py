f28da87501baab39c62c79e3d6130dbf94820a7f
Maga <uvaysov.2005@mail.ru>
2024-12-28 21:35:36 +0300

    Commenting

--- handlers.py
+++ handlers.py
@@ -14,8 +14,9 @@ async def handler(message: types.Message, command: CommandObject):
     session = aiohttp.ClientSession()
     back_request_url = f'{os.environ.get("BACKEND_ADDRESS")}tg/connect'
     data={"user": message.from_user.username, "token": token}
+    # в тело ответа вставляем 2 значения: user - никнейм пользователя в ТГ и token - идентификатор веб сокетамйки
     result = await session.post(url=back_request_url, data=json.dumps(data))
-    if result   .status == 200:
+    if result.status == 200: # если ответ на POST запрос имеет код статуса 200, то возвращаем уведомление об успешной авторизации, иначе - об обратномилс
         await message.answer("Авторизация прошла успешно, вы можете возвращаться обратно на страницу.")
     else:
         await message.answer("Произошла непредвиденная ошибка при попытке авторизации.")
